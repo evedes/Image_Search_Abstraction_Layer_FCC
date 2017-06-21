8d7c94489d35acfada0895e6166d27ea8fef01b2
Eduardo Vedes <eduardo.vedes@gmail.com>
2017-06-21 23:56:24 +0100

    upd db conn + packages
    updated db requirements and connections, updated routes

--- app.js
+++ app.js
@@ -1,12 +1,39 @@
+// Express and other Requirements
 const express = require('express')
 const app = express()
 const path = require('path')
+
+// Const and Var Definition
 const port = process.env.PORT | 8080;
+const route = require('./routes/routes.js')
 
-app.use('/', express.static(path.join(__dirname,'public')))
+// .env var definitions
+const dotenv = require('dotenv')
+dotenv.load()
+
+// Database Requirements
+const mongoose = require('mongoose')
+var urlToShorten = require('./db/connection.js')
+
+//Database Connection
+mongoose.connect('mongodb://' + process.env.DB_USER +':' + process.env.DB_PASS + process.env.DB_HOST)
+var db = mongoose.connection
+db.on('error',console.error.bind(console,'connection error:'))
+db.once('open', ()=>{
+    console.log('\nHey guys! We\'re connected!\n')
+})
 
 
 
+
+
+
+
+// app use forwarded to routes.js file
+app.use('/', route)
+
+
+//Server running and listening on const port
 app.listen(port,()=>{
     console.log('Server listening on port: ' + port)
 })
