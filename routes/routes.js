a427cf88a76b9e5ef4c9b70f68ada84526777cfc
Eduardo Vedes <eduardo.vedes@gmail.com>
2017-06-26 19:45:12 +0100

    JSON is okay
    Now it's missing to save search string and time to the databas

--- routes/routes.js
+++ routes/routes.js
@@ -9,17 +9,42 @@ dotenv.load()
 //ATTRIB STATIC PATH TO PUBLIC FOLDER
 router.use('/', express.static(path.join(__dirname,'../public')))
 
-router.get('/s/:query', (req,res)=>{
+router.get('/s/:query/offset=:n', (req,res)=>{
+    let results = []
+    let i = 0;
     query = req.params.query
-    url = 'https://pixabay.com/api/?key='+process.env.API_KEY+'&q='+ query +'&image_type=photo'
-    console.log(url)
-    getJSON(url, function(data){
-       res.json(data);
-    })  
+    n = req.params.n
+    
+    url = 'https://pixabay.com/api/?key='+process.env.API_KEY+'&q='+ query +'&image_type=photo&page=' + n
 
-       
+    getJSON(url, function(data){
+            
+            console.log(data.totalHits)
+            console.log(data.hits.length)
+
+            for (i = 0 ; i < data.hits.length ; i++) {
+            
+            results.push({
+                "Id": data.hits[i].id,
+                "Image Url": data.hits[i].webformatURL,
+                "Downloads": data.hits[i].downloads,
+            })
+            }
+    
+    
+
+
+    res.send(results)
+        
 })
 
+
+
+
+    
+  
+
+})
 
 
 module.exports = router
