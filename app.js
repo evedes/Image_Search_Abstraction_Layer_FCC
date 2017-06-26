af29948afda015d9b3c4dc5e896e67f48c09cb89
Eduardo Vedes <eduardo.vedes@gmail.com>
2017-06-26 23:32:21 +0100

    updated open to openUri

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ dotenv.load()
 mongoose.connect('mongodb://' + process.env.DB_USER +':' + process.env.DB_PASS + process.env.DB_HOST)
 var db = mongoose.connection
 db.on('error',console.error.bind(console,'connection error:'))
-db.once('open', ()=>{
+db.once('openUri', ()=>{
     console.log('\nHey guys! We\'re connected!\n')
 })
 
